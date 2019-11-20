7997867efc392d6fecd963137f6189fe1edf5aa6
LatinNear <50851888+LatinNear@users.noreply.github.com>
2019-11-19 22:46:52 -0600

    Add files via upload

---
+++ ubicacion.php
@@ -0,0 +1,18 @@
+<html> <head> <tittle><center><font size="10">Ubicacion de 
+ Maestros</font> </center></tittle> <head> <body 
+ background="texturamar.jpg"> <p><center> <font size="100">
+</font> </center> </p>
+<p align="center">Sistema de localizacion e identificación para docentes en 
+salones de clase y laboratorios</p>
+<p align="center">Este pagina sirve para conocer la ubicación de un docente
+ en específico dentro del campus en caso que se le necesite hacer 
+alguna consulta, por la revisión de un trabajo o por algún otro motivo académico. </p>
+<center><img src="itca.png" width="50%" height="50%">
+</center>
+<br>
+<center>
+<font size="5">Edificio B</font>
+</center>
+
+<center><div><font size "5">Se asigno al profesor: <?php include("pruebadetxt.txt");  ?></font> </div></center>
+<html>
